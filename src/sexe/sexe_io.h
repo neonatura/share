9a3dd1f7c6268494e31217a609c8db6cccd5f77c
Brian Burrell <bburrell.brian@gmail.com>
2019-04-20 22:00:47 -0600

    v3.2.3

--- src/sexe/sexe_io.h
+++ src/sexe/sexe_io.h
@@ -1,7 +1,7 @@
 
-void set_sexe_stdin(FILE *stdin);
-void set_sexe_stdout(FILE *stdout);
-void set_sexe_stderr(FILE *stderr);
+void set_sexe_stdin(FILE *in);
+void set_sexe_stdout(FILE *out);
+void set_sexe_stderr(FILE *err);
 
 FILE *get_sexe_stdin(void);
 FILE *get_sexe_stdout(void);
